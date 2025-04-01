78c48ab248f0290a1b2ef22d58fec5e04b5bc254
Muliika <samuelmuliika@gmail.com>
2025-04-01 19:09:35 +0300

    added runtime script

---
+++ boot/docker-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /opt/venv/bin/activate
+cd /code
+
+python manage.py sendtestemail --admins 
+python manage.py migrate --no-input 
+python manage.py auto_admin
+
+
+RUNTIME_PORT=${PORT:-8080}
+RUNTIME_HOST=${HOST:-0.0.0.0}
+
+# add our static files to container itself on runtime
+# python manage.py collectstatic --no-input
+gunicorn core.wsgi:application --bind $RUNTIME_HOST:$RUNTIME_PORT

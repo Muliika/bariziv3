680b5e2fe3793d39e749139870926203f4c75e7b
Muliika <samuelmuliika@gmail.com>
2025-04-01 18:38:09 +0300

    configured email

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,6 +15,31 @@ TEMPLATES_DIR.mkdir(parents=True, exist_ok=True)
 
 PROJECT_NAME = config("PROJECT_NAME", default="BariziUg")
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
+EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587")  # Recommended
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_USE_TLS = config(
+    "EMAIL_USE_TLS", cast=bool, default=True
+)  # Use EMAIL_PORT 587 for TLS
+EMAIL_USE_SSL = config(
+    "EMAIL_USE_SSL", cast=bool, default=False
+)  # EUse MAIL_PORT 465 for SSL
+
+# via gmail
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
+ADMIN_USER_NAME = config("ADMIN_USER_NAME", default="Admin user")
+ADMIN_USER_EMAIL = config("ADMIN_USER_EMAIL", default=None)
+
+MANAGERS = []
+ADMINS = []
+if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+    ADMINS += [(f"{ADMIN_USER_NAME}", f"{ADMIN_USER_EMAIL}")]
+    MANAGERS = ADMINS
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-v%zori0d%h3(w1&+2prrd5jcxn&ca)+x230&-$#9qf2^!fdea7"
